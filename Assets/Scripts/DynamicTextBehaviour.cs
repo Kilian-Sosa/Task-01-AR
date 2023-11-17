929bc1c9d0170c8d54c7f22f6d24849373b22240
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-17 17:46:40 +0000

    feat: Add Dynamic Text

---
+++ Assets/Scripts/DynamicTextBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class Function : MonoBehaviour {
+    public string text = "Bienvenido a FitBuddy, una aplicación dedicada a impulsar tu rutina de ejercicio! Descubre rutinas personalizadas, retos emocionantes y consejos de salud que te guiarán hacia una vida más activa. ¡Prepárate para transformar tu bienestar, un paso a la vez! #ActívateConNosotros";
+    //public TextMeshPro textBox; //Falla
+    public TMP_Text textBox;
+    public void ActivateText() {
+        StartCoroutine(ShowText());
+    }
+
+    public void DesactivateText() {
+        StopCoroutine("ShowText");
+        textBox.text = ""; //limpiar 
+    }
+    IEnumerator ShowText() {
+        for (int i = 0; i < text.Length; i++) {
+            textBox.text += text[i];
+            yield return new
+           WaitForSeconds(0.02f);
+        }
+    }
+}
