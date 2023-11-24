a87715efca56dbb26c2c2e98357d2c04fda82fc3
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-24 17:48:33 +0000

    fix: Solve Corroutine error

--- Assets/Scripts/DynamicTextBehaviour.cs
+++ Assets/Scripts/DynamicTextBehaviour.cs
@@ -6,16 +6,18 @@ public class Function : MonoBehaviour {
     public string text = "Bienvenido a FitBuddy, una aplicación dedicada a impulsar tu rutina de ejercicio! Descubre rutinas personalizadas, retos emocionantes y consejos de salud que te guiarán hacia una vida más activa. ¡Prepárate para transformar tu bienestar, un paso a la vez! #ActívateConNosotros";
     //public TextMeshPro textBox; //Falla
     public TMP_Text textBox;
+    private Coroutine showTextCoroutine;
     public void ActivateText() {
         StartCoroutine(ShowText());
+        showTextCoroutine = StartCoroutine(ShowText());
         textBox.GetComponent<AudioSource>().enabled = true;
     }
-
-    public void DesactivateText() {
-        StopCoroutine("ShowText");
+    public void DeactivateText() {
+        if (showTextCoroutine != null) StopCoroutine(showTextCoroutine);
         textBox.text = ""; //limpiar 
         textBox.GetComponent<AudioSource>().enabled = false;
     }
+        
     IEnumerator ShowText() {
         for (int i = 0; i < text.Length; i++) {
             textBox.text += text[i];
